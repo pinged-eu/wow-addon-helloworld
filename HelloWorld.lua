c1c2a334fe67ff75f589ab54b3b971bad0851bc7
Lexxy <lexxy23@users.noreply.github.com>
2023-03-05 09:42:57 +0100

    🌐 idea for i18n title and notes
    Also some hints on event handling.

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -1,3 +1,26 @@
 local name = UnitName("player")
 
+local f = CreateFrame("Frame")
+
+function f:OnEvent(event, ...)
+	self[event](self, event, ...)
+end
+
+function f:ADDON_LOADED(event, addOnName)
+	print(event, addOnName)
+end
+
+function f:PLAYER_ENTERING_WORLD(event, isLogin, isReload)
+	print(event, isLogin, isReload)
+end
+
+function f:CHAT_MSG_CHANNEL(event, text, playerName, _, channelName)
+	print(event, text, playerName, channelName)
+end
+
+f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("CHAT_MSG_CHANNEL")
+f:SetScript("OnEvent", OnEvent)
+
 message("Hello, " .. name .. "!")
