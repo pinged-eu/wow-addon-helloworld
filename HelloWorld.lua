23e230597c65f3f2a07a91ee52c86d517f328d4d
Rusty <rusty232@users.noreply.github.com>
2025-05-30 13:45:52 +0200

    wip: use downloaded ace3 libs from the pwsh script

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -6,32 +6,33 @@ local name = UnitName("player")
 local _, L = ...;
 
 function HelloWorld:OnInitialize()
-	-- Called when the addon is loaded
-	self:Print(L["hello"] .. name .. "!")
-	self:RegisterChatCommand("hw", "SlashCommand")
-	self:RegisterChatCommand("helloworld", "SlashCommand")
+    -- Called when the addon is loaded
+    self:Print(L["hello"] .. name .. "!")
+    self:RegisterChatCommand("hw", "SlashCommand")
+    self:RegisterChatCommand("helloworld", "SlashCommand")
+    self.db = LibStub("AceDB-3.0"):New("MyAddonDB")
 end
 
 function HelloWorld:SlashCommand(msg)
-	if msg == "ping" then
-		self:Print("pong!")
-	else
-		self:Print("hello there!")
-	end
+    if msg == "ping" then
+        self:Print("pong!")
+    else
+        self:Print("hello there!")
+    end
 end
 
 function HelloWorld:OnEnable()
-	self:RegisterEvent("ZONE_CHANGED")
+    self:RegisterEvent("ZONE_CHANGED")
 end
 
 function HelloWorld:OnDisable()
-	-- Called when the addon is disabled
+    -- Called when the addon is disabled
 end
 
 function HelloWorld:ZONE_CHANGED()
-	local subzone = GetSubZoneText()
-	self:Print("You have changed zones!", GetZoneText(), subzone)
-	if GetBindLocation() == subzone then
-		self:Print("Welcome Home!")
-	end
+    local subzone = GetSubZoneText()
+    self:Print("You have changed zones!", GetZoneText(), subzone)
+    if GetBindLocation() == subzone then
+        self:Print("Welcome Home!")
+    end
 end
