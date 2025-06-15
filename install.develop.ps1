875dd89d9ccd02c2f64cc23191353288ea21e659
Rusty <rusty232@users.noreply.github.com>
2025-06-15 13:43:22 +0200

    chore: allow hardlinks for develop mode

--- install.develop.ps1
+++ install.develop.ps1
@@ -1,7 +1,8 @@
 #!/usr/bin/env pwsh
 # This script installs the addon in development mode. Use -Information or -Verbose for logging.
 param(
-    [string]$wowDirectory = "C:\BNetGames\World of Warcraft"
+    [string]$wowDirectory = "C:\BNetGames\World of Warcraft",
+    [string]$linkType="SymbolicLink" # can be SymbolicLink, HardLink or Junction; Cata Classic seems to only take HardLinks for granted
 )
 
 # create method Test-IsAdmin; returns boolean
@@ -22,21 +23,21 @@ if (!(Test-Path "$wowDirectory")) {
     foreach ($flavor in $flavors) {
         # check if flavor directory exists
         if (!(Test-Path "$wowDirectory\$flavor")) {
-            Write-Information "WoW Directory $wowDirectory with $flavor flavor does not exist!"
+            Write-Host "WoW Directory $wowDirectory with $flavor flavor does not exist!"
             continue
         }
         $addonDir = "$wowDirectory\$flavor\Interface\AddOns\HelloWorld"
         if (Test-Path $addonDir) {
-            Write-Information "HelloWorld addon link already exists in $addonDir"
+            Write-Host "HelloWorld addon link already exists in $addonDir"
         } else {
             # check if user has administrator privileges
             if (-not (Test-IsAdmin)) {
                 Write-Error "You need administrator privileges to create symbolic links!"
             } else {
                 # create link
-                Write-Verbose "Creating symbolic link for HelloWorld addon..."
-                New-Item -ItemType SymbolicLink -Path "$wowDirectory\$flavor\Interface\AddOns\HelloWorld" -Target $myAddonDir
-                Write-Host "Symbolic link in $flavor created successfully!"
+                Write-Verbose "Creating link of type $linkType for HelloWorld addon..."
+                New-Item -ItemType $linkType -Path "$wowDirectory\$flavor\Interface\AddOns\HelloWorld" -Target $myAddonDir
+                Write-Host "Link in $flavor created successfully!"
             }
         }
     }
