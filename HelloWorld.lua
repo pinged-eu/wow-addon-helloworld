0ca6d80e62826aa90d934eb0d862be224e8260ab
Lexxy <lexxy23@users.noreply.github.com>
2025-07-20 09:21:27 +0200

    feat: config options for addon (#14)
    * wip: define ignores and attributes
    
    * chore(ace3): add pwsh script to download latest ace3 libs
    
    * wip: use downloaded ace3 libs from the pwsh script
    
    * wip: do not use stored vars yet
    
    * wip: crlf issue
    
    * docs: update docs to easier install the devel version of this addon
    
    * wip: try to make it run in Cata Classic
    
    * chore: allow hardlinks for develop mode
    
    * chore(ace3): allow other urls for ace3 libs
    
    * chore(locale): locales via Ace3 are working now
    
    * docs: add localization info
    
    * build: add editorconfig
    
    ---------
    
    Co-authored-by: Rusty <rusty232@users.noreply.github.com>

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -2,36 +2,45 @@ HelloWorld = LibStub("AceAddon-3.0"):NewAddon("HelloWorld", "AceConsole-3.0", "A
 
 local name = UnitName("player")
 
----@class L
-local _, L = ...;
+local L = LibStub("AceLocale-3.0"):GetLocale("HelloWorld")
 
 function HelloWorld:OnInitialize()
-	-- Called when the addon is loaded
-	self:Print(L["hello"] .. name .. "!")
-	self:RegisterChatCommand("hw", "SlashCommand")
-	self:RegisterChatCommand("helloworld", "SlashCommand")
+    -- Called when the addon is loaded
+    -- self:Print("onInit..")
+    self:Print(L["hello"](name))
+    self:RegisterChatCommand("hw", "SlashCommand")
+    self:RegisterChatCommand("helloworld", "SlashCommand")
 end
 
 function HelloWorld:SlashCommand(msg)
-	if msg == "ping" then
-		self:Print("pong!")
-	else
-		self:Print("hello there!")
-	end
+    if msg == "ping" then
+        self:Print("pong!")
+    else
+        self:Print("hello there!")
+    end
 end
 
 function HelloWorld:OnEnable()
-	self:RegisterEvent("ZONE_CHANGED")
+    -- self:Print("onEnable..")
+    self:RegisterEvent("ZONE_CHANGED")
 end
 
 function HelloWorld:OnDisable()
-	-- Called when the addon is disabled
+    -- Called when the addon is disabled
+    -- self:Print("onDisable..")
 end
 
 function HelloWorld:ZONE_CHANGED()
-	local subzone = GetSubZoneText()
-	self:Print("You have changed zones!", GetZoneText(), subzone)
-	if GetBindLocation() == subzone then
-		self:Print("Welcome Home!")
-	end
+    local thisZone = GetZoneText()
+    local subzone = GetSubZoneText()
+    -- self:Print("onZoneChanged..")
+    if subzone == "" then
+        self:Print(L["zoneChanged"](thisZone))
+    else
+        self:Print(L["zoneChangedSub"](thisZone, subzone))
+    end
+
+    if GetBindLocation() == subzone then
+        self:Print(L["welcomeHome"](name))
+    end
 end
---
+++ Libs/downloadAceLibs.ps1
@@ -0,0 +1,20 @@
+#!/usr/bin/env pwsh
+#
+param(
+    [string]$AceUrl = "https://www.wowace.com/projects/ace3/files/latest"
+    # Cata Classic: https://www.wowace.com/projects/ace3/files/6538965/download
+    # MoP Classic: https://www.wowace.com/projects/ace3/files/6538965/download
+)
+
+Write-Host "Downloading AceLibs from $AceUrl..."
+
+# # Download latest acelib from website, follow redirects
+Invoke-WebRequest -Uri $AceUrl -MaximumRedirection 10 -OutFile "$PSScriptRoot/AceLibs.zip" -UseBasicParsing
+
+Write-Host "Extracting AceLibs..."
+# Extract downloaded zip file
+Expand-Archive -Path "$PSScriptRoot/AceLibs.zip" -DestinationPath "$PSScriptRoot/" -Force
+
+Write-Host "Removing downloaded AceLibs.zip..."
+# Remove downloaded zip file
+Remove-Item -Path "$PSScriptRoot/AceLibs.zip"
