070dde5e7a641428789fc1cffebccd6a684e75b3
Lexxy <lexxy23@noreply.codeberg.org>
2025-12-28 16:59:38 +0100

    wip: idea to configure zone messaging

--- options/options.lua
+++ options/options.lua
@@ -13,6 +13,13 @@ local options = {
       set = function(info,val) HelloWorld.enabled = val end,
       get = function(info) return HelloWorld.enabled end
     },
+    enableZoneMessages = {
+      name = "Enable Zone Messages",
+      desc = "Enables / disables the zone messages",
+      type = "toggle",
+      set = function(info,val) HelloWorld.enableZoneMessage = val end,
+      get = function(info) return HelloWorld.enableZoneMessage end
+    },
   }
 }
 
